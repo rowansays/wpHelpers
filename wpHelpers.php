792cc600a98d62ba7074b940863fa34f6d556bcb
rowansays <74547960+rowansays@users.noreply.github.com>
2021-01-20 23:38:42 -0800

    Adding all files generated by usage: wp scaffold plugin <slug> [--dir=<dirname>] [--plugin_name=<title>] [--plugin_description=<description>] [--plugin_author=<author>] [--plugin_author_uri=<url>] [--plugin_uri=<url>] [--skip-tests] [--ci=<provider>] [--activate] [--activate-network] [--force].

---
+++ wpHelpers.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     WpHelpers
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wpHelpers
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         WpHelpers
+ */
+
+// Your code starts here.

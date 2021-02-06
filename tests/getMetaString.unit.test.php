99475fd7408fd1398a82415fbda178b31dafb1b6
rowansays <74547960+rowansays@users.noreply.github.com>
2021-02-06 00:19:29 -0800

    Tests: Do less work on each test.
    So... it looks like WP_UnitTestCase::setUp() runs immediately
    before each test, not before the test class. Now that that is
    known, we will split each test into its own class and only
    setup what is needed before running. Shout out to @w3guy for
    writing an article explaining what is happening here :)
    https://www.smashingmagazine.com/2017/12/automated-testing-wordpress-plugins-phpunit/

--- tests/getMetaString.unit.test.php
+++ tests/getMetaString.unit.test.php
@@ -10,7 +10,7 @@ use function RowanSaysWpHelpers\Meta\getMetaString;
 
 require_once dirname(__DIR__) . '/Meta/Meta.php';
 
-class TestGetMetaStringFromPost extends WP_UnitTestCase {
+class TestGetMetaString_Post_NonExistent extends WP_UnitTestCase {
   private int $postId = 0;
   public function setUp(): void {
     $id = wp_insert_post([
@@ -18,54 +18,10 @@ class TestGetMetaStringFromPost extends WP_UnitTestCase {
       'post_content' => 'This is just some sample post content.'
     ]);
 
-    update_post_meta($id, '_wpHelpersNull', null);
-    update_post_meta($id, '_wpHelpersInt', 123);
-    update_post_meta($id, '_wpHelpersFloat', 1.23);
-    update_post_meta($id, '_wpHelpersString', 'ABC');
-    update_post_meta($id, '_wpHelpersArray', [1, 2, 3]);
-    update_post_meta($id, '_wpHelpersObject', (object) ['A' => 1, 'B' => 2, 'C' => 3]);
-
     $this->postId = $id;
   }
   public function tearDown(): void {
    wp_delete_post($this->postId, true);
-  }
-  public function test_itPassesWhenMetaWasInsertedAsInt () {
-    $getMeta = getMetaString('post', $this->postId, '_wpHelpersInt');
-    print_r($getMeta->toMarkdown());
-    $this->assertTrue($getMeta->passed());
-    $this->assertEquals('123', $getMeta->toValue());
-  }
-  public function test_itPassesWhenMetaWasInsertedAsFloat () {
-    $getMeta = getMetaString('post', $this->postId, '_wpHelpersFloat');
-    $this->assertTrue($getMeta->passed());
-    $this->assertEquals('1.23', $getMeta->toValue());
-  }
-  public function test_itPassesWhenMetaWasInsertedAsString () {
-    $getMeta = getMetaString('post', $this->postId, '_wpHelpersString');
-    $this->assertTrue($getMeta->passed());
-    $this->assertEquals('ABC', $getMeta->toValue());
-  }
-  public function test_itFailsWhenMetaWasInsertedAsArray () {
-    $getMeta = getMetaString('post', $this->postId, '_wpHelpersArray');
-    $this->assertTrue($getMeta->failed());
-    $this->assertEquals(1, count($getMeta));
-    $this->assertEquals('', $getMeta->toValue());
-    $this->assertTrue($getMeta->failed('invalidType'));
-  }
-  public function test_itFailsWhenMetaWasInsertedAsObject () {
-    $getMeta = getMetaString('post', $this->postId, '_wpHelpersObject');
-    $this->assertTrue($getMeta->failed());
-    $this->assertEquals(1, count($getMeta));
-    $this->assertEquals('', $getMeta->toValue());
-    $this->assertTrue($getMeta->failed('invalidType'));
-  }
-  public function test_itFailsWhenMetaWasInsertedAsNull () {
-    $getMeta = getMetaString('post', $this->postId, '_wpHelpersNull');
-    $this->assertTrue($getMeta->failed());
-    $this->assertEquals(1, count($getMeta));
-    $this->assertEquals('', $getMeta->toValue());
-    $this->assertTrue($getMeta->failed('emptyValue'));
   }
   public function test_itFailsWhenMetaDoesNotExist () {
     $getMeta = getMetaString('post', $this->postId, '_iDoNotExist');
@@ -75,66 +31,136 @@ class TestGetMetaStringFromPost extends WP_UnitTestCase {
     $this->assertTrue($getMeta->failed('emptyValue'));
   }
 }
-
-class TestGetMetaStringFromTerm extends WP_UnitTestCase {
-  private int $termId = 0;
+class TestGetMetaString_Post_Null extends WP_UnitTestCase {
+  private int $postId = 0;
   public function setUp(): void {
-    $insertTerm = wp_insert_term('Sample Term123', 'post_tag');
-    $id = $insertTerm['term_id'];
+    $id = wp_insert_post([
+      'post_title' => 'Sample Post',
+      'post_content' => 'This is just some sample post content.'
+    ]);
 
-    update_term_meta($id, '_wpHelpersNull', null);
-    update_term_meta($id, '_wpHelpersInt', 123);
-    update_term_meta($id, '_wpHelpersFloat', 1.23);
-    update_term_meta($id, '_wpHelpersString', 'ABC');
-    update_term_meta($id, '_wpHelpersArray', [1, 2, 3]);
-    update_term_meta($id, '_wpHelpersObject', (object) ['A' => 1, 'B' => 2, 'C' => 3]);
+    update_post_meta($id, '_wpHelpersNull', null);
 
-    $this->termId = $id;
+    $this->postId = $id;
   }
-  public function tearDown() : void {
-   wp_delete_term($this->termId, 'post_tag');
+  public function tearDown(): void {
+   wp_delete_post($this->postId, true);
+  }
+  public function test_itFailsWhenMetaWasInsertedAsNull () {
+    $getMeta = getMetaString('post', $this->postId, '_wpHelpersNull');
+    $this->assertTrue($getMeta->failed());
+    $this->assertEquals(1, count($getMeta));
+    $this->assertEquals('', $getMeta->toValue());
+    $this->assertTrue($getMeta->failed('emptyValue'));
+  }
+}
+class TestGetMetaString_Post_Int extends WP_UnitTestCase {
+  private int $postId = 0;
+  public function setUp(): void {
+    $id = wp_insert_post([
+      'post_title' => 'Sample Post',
+      'post_content' => 'This is just some sample post content.'
+    ]);
+
+    update_post_meta($id, '_wpHelpersInt', 123);
+
+    $this->postId = $id;
+  }
+  public function tearDown(): void {
+   wp_delete_post($this->postId, true);
   }
   public function test_itPassesWhenMetaWasInsertedAsInt () {
-    $getMeta = getMetaString('term', $this->termId, '_wpHelpersInt');
+    $getMeta = getMetaString('post', $this->postId, '_wpHelpersInt');
     $this->assertTrue($getMeta->passed());
     $this->assertEquals('123', $getMeta->toValue());
   }
+}
+class TestGetMetaString_Post_Float extends WP_UnitTestCase {
+  private int $postId = 0;
+  public function setUp(): void {
+    $id = wp_insert_post([
+      'post_title' => 'Sample Post',
+      'post_content' => 'This is just some sample post content.'
+    ]);
+
+    update_post_meta($id, '_wpHelpersFloat', 1.23);
+
+    $this->postId = $id;
+  }
+  public function tearDown(): void {
+   wp_delete_post($this->postId, true);
+  }
   public function test_itPassesWhenMetaWasInsertedAsFloat () {
-    $getMeta = getMetaString('term', $this->termId, '_wpHelpersFloat');
+    $getMeta = getMetaString('post', $this->postId, '_wpHelpersFloat');
     $this->assertTrue($getMeta->passed());
     $this->assertEquals('1.23', $getMeta->toValue());
   }
+}
+class TestGetMetaString_Post_String extends WP_UnitTestCase {
+  private int $postId = 0;
+  public function setUp(): void {
+    $id = wp_insert_post([
+      'post_title' => 'Sample Post',
+      'post_content' => 'This is just some sample post content.'
+    ]);
+
+    update_post_meta($id, '_wpHelpersString', 'ABC');
+
+    $this->postId = $id;
+  }
+  public function tearDown(): void {
+   wp_delete_post($this->postId, true);
+  }
   public function test_itPassesWhenMetaWasInsertedAsString () {
-    $getMeta = getMetaString('term', $this->termId, '_wpHelpersString');
+    $getMeta = getMetaString('post', $this->postId, '_wpHelpersString');
     $this->assertTrue($getMeta->passed());
     $this->assertEquals('ABC', $getMeta->toValue());
   }
+}
+class TestGetMetaString_Post_Array extends WP_UnitTestCase {
+  private int $postId = 0;
+  public function setUp(): void {
+    $id = wp_insert_post([
+      'post_title' => 'Sample Post',
+      'post_content' => 'This is just some sample post content.'
+    ]);
+
+    update_post_meta($id, '_wpHelpersArray', [1, 2, 3]);
+    update_post_meta($id, '_wpHelpersObject', (object) ['A' => 1, 'B' => 2, 'C' => 3]);
+
+    $this->postId = $id;
+  }
+  public function tearDown(): void {
+   wp_delete_post($this->postId, true);
+  }
   public function test_itFailsWhenMetaWasInsertedAsArray () {
-    $getMeta = getMetaString('term', $this->termId, '_wpHelpersArray');
+    $getMeta = getMetaString('post', $this->postId, '_wpHelpersArray');
     $this->assertTrue($getMeta->failed());
     $this->assertEquals(1, count($getMeta));
     $this->assertEquals('', $getMeta->toValue());
     $this->assertTrue($getMeta->failed('invalidType'));
+  }
+}
+class TestGetMetaString_Post_Object extends WP_UnitTestCase {
+  private int $postId = 0;
+  public function setUp(): void {
+    $id = wp_insert_post([
+      'post_title' => 'Sample Post',
+      'post_content' => 'This is just some sample post content.'
+    ]);
+
+    update_post_meta($id, '_wpHelpersObject', (object) ['A' => 1, 'B' => 2, 'C' => 3]);
+
+    $this->postId = $id;
+  }
+  public function tearDown(): void {
+   wp_delete_post($this->postId, true);
   }
   public function test_itFailsWhenMetaWasInsertedAsObject () {
-    $getMeta = getMetaString('term', $this->termId, '_wpHelpersObject');
+    $getMeta = getMetaString('post', $this->postId, '_wpHelpersObject');
     $this->assertTrue($getMeta->failed());
     $this->assertEquals(1, count($getMeta));
     $this->assertEquals('', $getMeta->toValue());
     $this->assertTrue($getMeta->failed('invalidType'));
-  }
-  public function test_itFailsWhenMetaWasInsertedAsNull () {
-    $getMeta = getMetaString('term', $this->termId, '_wpHelpersNull');
-    $this->assertTrue($getMeta->failed());
-    $this->assertEquals(1, count($getMeta));
-    $this->assertEquals('', $getMeta->toValue());
-    $this->assertTrue($getMeta->failed('emptyValue'));
-  }
-  public function test_itFailsWhenMetaDoesNotExist () {
-    $getMeta = getMetaString('term', $this->termId, '_iDoNotExist');
-    $this->assertTrue($getMeta->failed());
-    $this->assertEquals(1, count($getMeta));
-    $this->assertEquals('', $getMeta->toValue());
-    $this->assertTrue($getMeta->failed('emptyValue'));
   }
 }
